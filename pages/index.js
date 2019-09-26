7d8846903070fa0a3129339617ef2d5fa159fb92
Simon Mai <maismin@gmail.com>
2019-09-26 14:03:33 -0400

    Create api route for products

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
-function Home() {
-  return <>home</>;
+import { useEffect } from 'react'
+import axios from 'axios'
+
+const Home = () => {
+  useEffect(() => {
+    getProducts()
+  }, [])
+
+  const getProducts = async () => {
+    const url = 'http://localhost:3000/api/products'
+    const response = await axios.get(url)
+    console.log(response.data)
+  }
+
+  return <>home</>
 }
 
-export default Home;
+export default Home
