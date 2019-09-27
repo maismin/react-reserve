5d073c22321155ec3c5c9e11c8cec9bb539a9483
Simon Mai <maismin@gmail.com>
2019-09-27 12:10:32 -0400

    Create a base url helper

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react'
 import axios from 'axios'
 import ProductList from '../components/Index/ProductList'
+import baseUrl from '../utils/baseUrl'
 
 const Home = ({ products }) => {
   return <ProductList products={products} />
@@ -10,7 +11,7 @@ Home.getInitialProps = async () => {
   // fetch data on server
   // return response data as an object
   // this object is merged with existing props
-  const url = 'http://localhost:3000/api/products'
+  const url = `${baseUrl}/api/products`
   const response = await axios.get(url)
   return { products: response.data }
 }
