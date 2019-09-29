6f52bed7f296be0cc7846a8bf355af63151ad84e
Simon Mai <maismin@gmail.com>
2019-09-29 09:20:03 -0400

    Fix AddProductToCart component to keep track of quantity of item

--- components/Product/AddProductToCart.js
+++ components/Product/AddProductToCart.js
@@ -1,11 +1,15 @@
 import { Input } from 'semantic-ui-react'
+import { useState } from 'react'
 
 const AddProductToCart = () => {
+  const [quantity, setQuantity] = useState(1)
+
   return (
     <Input
       type="number"
       min="1"
-      value={1}
+      value={quantity}
+      onChange={event => setQuantity(Number(event.target.value))}
       placeholder="Quantity"
       action={{
         color: 'orange',
